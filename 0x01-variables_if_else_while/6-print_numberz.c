428729cd07f31adcd0d60e8d0f6bb322abe4aba6
Alexandria <alexandraalethea@gmail.com>
2017-05-12 01:24:48 +0000

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - this is a description
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar (i);
+	}
+
+	putchar (10);
+	return (0);
+}
