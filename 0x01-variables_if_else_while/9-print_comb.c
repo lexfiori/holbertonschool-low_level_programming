0930373a6ccda193d6d933d4a5bdf54c13134a21
Alexandria <alexandraalethea@gmail.com>
2017-05-12 03:22:39 +0000

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,16 @@
 
 int main(void)
 {
-	char c
-		for (c = 48; c <= 57; c++)
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c != 57)
 		{
-			if ( c != 57);
-			{
-				putchar (32);
-				putchar (44);
-			}
-			putchar (c);
+			putchar (32);
+			putchar (44);
 		}
+			putchar (c);
+	}
 	return (0);
 }
