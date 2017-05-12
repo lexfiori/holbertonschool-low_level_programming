02bc92c8dee2ca575d1cb6179b4fae9803125f84
Alexandria <alexandraalethea@gmail.com>
2017-05-12 01:30:59 +0000

    smile in a mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - this is a
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 57; c >= 48; c--)
+	{
+		putchar (c);
+	}
+	putchar ('\n');
+	return (0);
+}
