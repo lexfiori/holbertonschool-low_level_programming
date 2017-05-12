060a92469f9be793dca594b15bc0368792ece922
Alexandria <alexandraalethea@gmail.com>
2017-05-12 05:12:07 +0000

    deleted spaces

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,13 +15,13 @@ int main(void)
 
 	for (c = 48; c < 58; c++)
 	{
-		putchar (c);
+		putchar(c);
 		if (c != 57)
 		{
-			putchar (44);
-			putchar (32);
+			putchar(44);
+			putchar(32);
 		}
 	}
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
