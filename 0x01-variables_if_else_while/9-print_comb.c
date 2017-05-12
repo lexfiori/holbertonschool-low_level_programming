3e807ed6d7081ec1755f2f389ccebf7f1164f5c5
Alexandria <alexandraalethea@gmail.com>
2017-05-12 03:07:49 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - this is a
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c
+		for (c = 48; c <= 57; c++)
+		{
+			if ( c != 57);
+			{
+				putchar (32);
+				putchar (44);
+			}
+			putchar (c);
+		}
+	return (0);
+}
