f37b08a68798cd1ea1543798badb2341cde53edf
Alexandria <alexandraalethea@gmail.com>
2017-05-12 00:43:51 +0000

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+		{
+			c++;
+		}
+		putchar (c);
+		}
+	putchar (10);
+	return (0);
+}
