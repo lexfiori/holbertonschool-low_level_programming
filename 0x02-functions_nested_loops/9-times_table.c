903ff3d52168b1d6c8c74f6bca78958c3a55caee
Alexandria Farrar <alexandriaalethea@gmail.com>
2017-05-17 04:20:31 +0000

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,36 +1,45 @@
 #include "holberton.h"
 
+/**
+ * times_table - prints letters in ASCII
+ *
+(* Description: lowercase letters
+(*
+* Return: void
+*/
+
 void times_table(void)
 {
 	int a;
 	int b;
 	int c;
-	
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			c = a * b; 
-			
-			if (c > 10)
+			c = a * b;
+
+			if (c >= 10)
 			{
+				_putchar(',');
 				_putchar(' ');
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
-				_putchar(',');
 			}
+
 			if (c < 10 && b != 0)
 			{
+				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar((c % 10) + '0');
-				_putchar(',');
-			}
+							}
 			if (b == 0)
 			{
 				_putchar(c + '0');
-				_putchar(',');
 			}
 		}
+		_putchar('\n');
 	}
 }
