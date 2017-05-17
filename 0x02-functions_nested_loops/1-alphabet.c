dd1ca4c80d739ee8ae0eeedb19b416c1f0345a8f
Alexandria Farrar <alexandriaalethea@gmail.com>
2017-05-17 00:43:00 +0000

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,19 @@
 
 /**
  * print_alphabet - print alphabet
- * 
  *
- * Description: prints the alphabet
- * section header: void print_alphabet(void))*
- * Return: 0 
+ *Description - prints the alphabet
+ *
+ * Return: 0
  */
 
 void print_alphabet(void)
 {
-        char c;
-        for (c = 'a'; c <= 'z'; c++)
-        {
-                _putchar(c);
-        }
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
 	_putchar('\n');
 }
