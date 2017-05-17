f347c980c2ffa3680ff5f960aca766546e135813
Alexandria Farrar <alexandriaalethea@gmail.com>
2017-05-17 02:16:14 +0000

    7 print_last_digit fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,35 +2,33 @@
 
 /**
  * print_last_digit - prints last digit
+ *
  * @n: an integer
-(*
-* Description: prints last digit
-(* section header: Section description)*
-* Return: j
-*/
+ *
+ * Return: j
+ */
 
 int print_last_digit(int n)
 {
 	int j;
 
-	if (n > 0)
+	if (n != 0)
 	{
 		j = n % 10;
-		_putchar(j + '0');
 
-	}
+		if (n < 0)
+		{
+			j = -j;
+		}
 
-	else if (n < 0)
-	{
-		n = -n;
-		j = n % 10;
 		_putchar(j + '0');
 	}
+
 
 	else
 	{
 		j = 0;
-		_putchar(n + '0');
+		_putchar(j + '0');
 	}
 	return (j);
 }
