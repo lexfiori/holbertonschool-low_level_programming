83a7d1e33ab76702d03f21425b6e4b134084c8a6
Alexandria Farrar <alexandriaalethea@gmail.com>
2017-05-17 23:36:52 +0000

    102 fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry of main
+ *
+ * Description - this is a description of main
+ *
+ * Return: 0 - this is the return
+ *
+ */
+
+int main(void)
+{
+	unsigned int i;
+	unsigned int n = 1;
+	unsigned int j = 0;
+
+	for (i = 1; i <= 50; i++)
+	{
+		unsigned int l;
+
+		l = j;
+		j = n + j;
+		n = l;
+
+		printf("%u\n", j);
+	}
+	return (0);
+}
+
+
