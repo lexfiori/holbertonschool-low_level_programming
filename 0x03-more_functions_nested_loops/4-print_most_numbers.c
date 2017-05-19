f70ad5b6119cb92b39fe762f7772ff98bf5e0899
lexfiori <alexandriaalethea@gmail.com>
2017-05-19 01:06:43 +0000

    fixed betty tabs

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,11 +12,11 @@ void print_most_numbers(void)
 {
 	int c;
 
-	for (c = 48; c<= 57; c++)
+	for (c = 48; c <= 57; c++)
 	{
 		if (c != 50 || c != 52)
 		{
-		       _putchar(c);
+			_putchar(c);
 		}
 		else
 		{
