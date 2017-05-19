c9904423ca51400380432b5262e43e86e0b9230a
lexfiori <alexandriaalethea@gmail.com>
2017-05-19 01:11:47 +0000

    fixed the output

--- 0x03-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,13 +14,9 @@ void print_most_numbers(void)
 
 	for (c = 48; c <= 57; c++)
 	{
-		if (c != 50 || c != 52)
+		if (c != 50 && c != 52)
 		{
 			_putchar(c);
-		}
-		else
-		{
-			c++;
 		}
 	}
 	_putchar('\n');
