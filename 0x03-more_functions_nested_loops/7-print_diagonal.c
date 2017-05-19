240170ec08422759ae9e72e7bfc7083dcdb7bc46
lexfiori <alexandriaalethea@gmail.com>
2017-05-19 05:39:09 +0000

    diagonals

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -14,12 +14,21 @@ void print_diagonal(int n)
 	int j;
 	int l;
 
-	for (l = 0; l < j; l++)
+	if (n > 0)
 	{
-		for (j = 0; j < n; j++)
+		for (l = 0; l < n; l++)
 		{
+			for (j = 0; j < l; j++)
+			{
+				_putchar(' ');
+			}
+
+			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar('_');
 	}
-	_putchar('\n');
+	else if (n <= 0)
+	{
+		_putchar('\n');
+	}
 }
