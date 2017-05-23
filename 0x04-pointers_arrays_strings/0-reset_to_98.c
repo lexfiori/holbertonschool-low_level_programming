945d510dfd5410679c84e37e8b05c81c4665f386
Alexandria Farrar <alexandriaalethea@gmail.com>
2017-05-23 18:45:46 +0000

    0 to 98

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,12 @@
 
 /**
  * reset_to_98 - pointers, first code
- * @*n: takes a pointer
+ * @n: takes a pointer
  *
  * Return: Always 0
  */
 
 void  reset_to_98(int *n)
 {
-        *n = 98;
+	*n = 98;
 }
