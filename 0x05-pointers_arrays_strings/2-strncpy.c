601547629bb1d5a106957b728ca8caa53fa79fa1
Alexandria Farrar <alexandriaalethea@gmail.com>
2017-05-25 22:24:42 +0000

    element matcher

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,26 @@
 #include "holberton.h"
 
+/**
+ * _strncpy - copies the content of a string up to an n
+ *
+ * @dest: the first string that will have src copied into it
+ * @src: the second string that will be put into dest
+ * @n: the amount of elements that will be copied from src to dest
+ * Return: dest
+ */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len, intmatcher;
+	int elmatch;
 
-	for
+	for (elmatch = 0; elmatch < n && src[elmatch] != '\0'; elmatch++)
+	{
+		dest[elmatch] = src[elmatch];
+	}
+	while (elmatch < n)
+	{
+		dest[elmatch] = '\0';
+		elmatch++;
+	}
+	return (dest);
+}
