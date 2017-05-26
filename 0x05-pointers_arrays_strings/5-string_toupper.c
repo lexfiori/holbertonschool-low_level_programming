c33eb27e978f61f20f44d1853376ba4412acf4d3
Alexandria Farrar <alexandriaalethea@gmail.com>
2017-05-26 04:24:44 +0000

    Holberton school

--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -1,17 +1,24 @@
 #include "holberton.h"
 
+/**
+ * string_toupper - change the string to upper
+ * @s - pointer made up of chars
+ *
+ * Return: 0
+ */
+
 char *string_toupper(char *s)
 {
 	int i, j;
 
-	while(s[i])
+	while (s[i])
 	{
 		i++;
 	}
 
-	for(j = 0; j < i; j++)
+	for (j = 0; j < i; j++)
 	{
-		if(s[j] >= 97 && s[j] <= 122)
+		if (s[j] >= 97 && s[j] <= 122)
 		{
 			s[j] = s[j] - 32;
 		}
