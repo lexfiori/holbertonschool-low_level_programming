2286138783651b4dd1457a008242a1a8402c910e
Alexandria <alexandraalethea@gmail.com>
2017-05-31 04:07:20 +0000

    3-strspn fixed betty

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,13 @@
 #include "holberton.h"
 
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ * @s: a string of characters
+ * @accept: a string of characters
+ *
+ * Return: bytes
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	int n, j, bytes;
@@ -7,7 +15,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (n = 0; s[n] != '\0'; n++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
- 		{
+		{
 			if (s[n] == accept[j])
 			{
 				bytes++;
