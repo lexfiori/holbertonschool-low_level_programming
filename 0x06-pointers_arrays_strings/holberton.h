e8a1bb4d51a76e6f6505233340380bc0a73a4677
Alexandria <alexandraalethea@gmail.com>
2017-05-31 03:04:33 +0000

    1-memcpy.c

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,1 +1,4 @@
 char *_memset(char *s, char b, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int size);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
