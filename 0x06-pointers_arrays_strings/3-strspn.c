a58b5c6042e191881527371a5dca76a9fb8a93bb
Alexandria <alexandraalethea@gmail.com>
2017-05-31 04:05:07 +0000

    3-strspn.c

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -1,40 +1,23 @@
 #include "holberton.h"
 
-/**
- * _strspn - length of the prefix of a substring
- * @s: a pointer to a string of characters
- * @accept: a pointer to a string of characters
- *
- * Return: the count
- */
-
 unsigned int _strspn(char *s, char *accept)
 {
-	int bytes = 0;
+	int n, j, bytes;
 
-	while(*s && *_strchr(accept, *s++))
+	for (n = 0; s[n] != '\0'; n++)
 	{
-		bytes++;
+		for (j = 0; accept[j] != '\0'; j++)
+ 		{
+			if (s[n] == accept[j])
+			{
+				bytes++;
+				break;
+			}
+			if (s[n] != accept[j])
+			{
+				break;
+			}
+		}
 	}
 	return (bytes);
 }
-
-/**
- * *_strchr - the same
- * @s: is a pointer to a string
- * @c: a character
- *
- * Return: 0
- */
-
-
-char *_strchr(char *s, char c)
-{
-while (*s != (char)c)
-                if (!*s++)
-                {
-                        return (0);
-                }
-        return (s);
-}
-
