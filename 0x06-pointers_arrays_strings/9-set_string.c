d0af59138723c77ca55ce49c3abb716e227b1c1a
Alexandria Farrar <alexandriaalethea@gmail.com>
2017-05-31 21:20:12 +0000

    9 set string

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets
+ * @s: a double pointer
+ * @to: single character pointer
+ *
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
