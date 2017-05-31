24a5fb9979cc8f8899e9fdd42f40dda97497bd6a
Alexandria <alexandraalethea@gmail.com>
2017-05-31 03:28:35 +0000

    betty doesn't like casting

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -1,15 +1,18 @@
 #include "holberton.h"
 
+/**
+ * *_strchr - a function that locates a character on a string
+ *
+ * @s: pointer to a string
+ * @c: pointer to a char
+ */
+
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
+	while (*s != (char)c)
+		if (!*s++)
 		{
-			return (s[i]);
+			return (0);
 		}
-	}
-	return ('\0');
+	return (char *)s;
 }
