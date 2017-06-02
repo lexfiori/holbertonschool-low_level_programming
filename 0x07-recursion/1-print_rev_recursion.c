8fb21cf1f35c476cc7b3d73ad949ed2226da0f93
Alexandria <alexandriaalethea@gmail.com>
2017-06-02 01:52:11 +0000

    1-print_rev

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - print rev recursion
+ * @s: a string s
+ *
+ * Return: 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	if (s[0] != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(s[0]);
+	}
+}
