ffd37a226f5f7ff28c328503028c18c479dab866
Alexandria <alexandriaalethea@gmail.com>
2017-06-02 03:50:43 +0000

    3-factorial

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * factorial - return a factorial programmed using recursion
+ * @n: a number to be put into the factorial program
+ *
+ * Return: 0
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (0);
+	}
+
+	if (n == 1 || n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+	return (0);
+}
