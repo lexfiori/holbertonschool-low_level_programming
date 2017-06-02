cc7c825c398ae6f742a1aec8fb0ba32a6d81c316
Alexandria <alexandriaalethea@gmail.com>
2017-06-02 01:34:31 +0000

    0 puts recursion

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -10,15 +10,13 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	if (s[0] != '\0')
 	{
-		_putchar(s[i]);
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
 	}
-	if (s[i] == '\0')
+	else
 	{
 		_putchar(10);
 	}
-
 }
